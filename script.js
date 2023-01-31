8e3e046b2f878991927318197f562ab553b5d794
Sergey Vorobev <ch4rlythec4t@gmail.com>
2023-01-31 18:20:17 +0300

    Init commit

---
+++ script.js
@@ -0,0 +1,60 @@
+// Task 1
+//   let arr = [1, 2, 3, 4, 5, 6, 7]
+
+//   for (let index = 0; index < arr.length; index++) {
+//       console.log(`arr [${[index]}] ${arr[index]}`)
+//   }
+
+// Task 2
+// let arr = [`a`, `b`, `c`]
+
+// console.log(arr)
+// arr[0] = 1
+// arr[1] = 2
+// arr[2] = 3
+
+// console.log(arr)
+
+
+// Task 3
+// let arr = [1, 2, 3, 4, 5, 6, 7]
+
+// console.log(arr)
+
+// for (let index = 0; index < arr.length; index++) {
+//     arr[index]++
+// }
+// console.log(arr)
+
+// Task 4
+// let arr = []
+
+// console.log(arr.length)
+// arr[3] = `a`
+// console.log(arr.length)
+
+// Task 5
+// let arr = [1, 2, 3]
+
+// console.log(arr)
+// arr.push(4)
+// arr.push(5)
+// console.log(arr)
+
+// Task 6
+// let arr = [1, 2, 3, 4, 5]
+
+// console.log(arr)
+// console.log(arr.length)
+// arr.splice(2, 2)
+
+// console.log(arr)
+// console.log(arr.length)
+
+// Task 6
+// let sum = `-`
+// for (let index = 0; index <= 9; index++) {
+//     sum += `${index}-`;
+// }
+
+// console.log(sum)
